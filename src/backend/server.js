9a181a761b9a4504945da81330cdf7207c495c66
nilan01 <nilanpatel33@hotmail.com>
2020-12-14 15:07:21 -0500

    single command app-start via concurrently

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,14 +3,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get("/api/menuitems", (req, res) => {
-    var menuItems = [
-        {id: 1, description: "Chocolate Cheesecake"},
-        {id: 2, description: "Strawberry Cheesecake"},
-        {id: 3, description: "Vanilla Cheesecake"},
-    ];
-    res.json(menuItems);
-})
+app.get('/api/menuitems', (req, res) => {
+  var menuItems = [
+    { id: 1, description: 'Chocolate Cheesecake' },
+    { id: 2, description: 'Strawberry Cheesecake' },
+    { id: 3, description: 'Vanilla Cheesecake' },
+  ];
+  res.json(menuItems);
+});
 
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
