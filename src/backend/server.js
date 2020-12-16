7bc14a2f27da9eb6734519e7697ffc6b1f1d6447
nilan01 <nilanpatel33@hotmail.com>
2020-12-16 13:41:24 -0500

    added dev and prod start scripts

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,14 @@
 const { json } = require('express');
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 app.get('/api/menuitems', (req, res) => {
+  console.log('hi there');
   var menuItems = [
     { id: 1, description: 'Chocolate Cheesecake' },
     { id: 2, description: 'Strawberry Cheesecake' },
