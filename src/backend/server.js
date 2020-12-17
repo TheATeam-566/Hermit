f120134b4d923bacec63c7f1c2e537cad778122e
nilan01 <nilanpatel33@hotmail.com>
2020-12-17 16:47:42 -0500

    added express skeleton, fixed proxy address, added beer

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,21 +1,13 @@
-const { json } = require('express');
 const express = require('express');
+const menuRouter = require('./routes/menuitems');
+const drinkRouter = require('./routes/drinks');
 
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.get('/api/menuitems', (req, res) => {
-  console.log('hi there');
-  const menuItems = [
-    { id: 1, description: 'Chocolate Cheesecake' },
-    { id: 2, description: 'Strawberry Cheesecake' },
-    { id: 3, description: 'Vanilla Cheesecake' },
-  ];
-  res.json(menuItems);
-});
+app.use('/menuitems', menuRouter);
+app.use('/drinkitems', drinkRouter);
 
 app.listen(port, () => console.log(`Express server started on port ${port}`));
+
+module.exports = app;
