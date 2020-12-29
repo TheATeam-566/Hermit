f7e76fa0e3af8a84dd6515038b6b87556a9fafe4
Chris Pinkney <hey@chrispinkney.com>
2020-12-29 15:40:50 -0500

    added basic footer

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Menuitems from './components/Menuitems';
+import Footer from './components/Footer';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Menuitems />
+        <Footer />
       </div>
     );
   }
---
+++ src/frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+        <div class="container text-center">
+          <small>Copyright 2020 The A Team</small>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
