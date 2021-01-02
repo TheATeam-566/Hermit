e1b3e588ef1cc9043ef9924219338e62491acbda
Nilan Patel <nilanpatel33@hotmail.com>
2021-01-02 10:58:08 -0500

    review changes for PR #79

--- src/frontend/src/components/App.js
+++ src/frontend/src/components/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
       <div>
         <Header />
         {/*<Menuitems />*/}
-        <Mainpage></Mainpage>
+        <Mainpage />
         <Footer />
       </div>
     );
