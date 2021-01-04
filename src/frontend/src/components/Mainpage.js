8e2b4621b31fb93df281478193470eeb5f597f3e
Chris Pinkney <hey@chrispinkney.com>
2021-01-04 11:18:38 -0500

    touching up styling before deploying on prod

--- src/frontend/src/components/Mainpage.js
+++ src/frontend/src/components/Mainpage.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import Categories from './Categories';
+import { Container, Col, Row } from 'react-bootstrap';
 
-export default class Mainpage extends Component {
+class Mainpage extends Component {
   // https://www.golangprograms.com/how-to-pass-data-from-child-component-to-its-parent-in-reactjs.html
   // See the link above for descriptions on the multiple ways to pass data from child to parent components.
   // Note, we will need to pass data from Categories back to Mainpage in order to then render the Items component.
 
   render() {
     return (
-      <div>
-        <Categories />
+      <div className="mainpage">
+        <Container fluid>
+          <Row>
+            <Col xs={3}>
+              <Categories />
+            </Col>
+            <Col xs={9}>Items Go Here.</Col>
+          </Row>
+        </Container>
       </div>
     );
   }
 }
+
+export default Mainpage;
